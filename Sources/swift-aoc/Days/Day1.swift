f7cc9c706574312c70d013699cb233c1139d6340
Michael Selsky <michael@selsky.me>
2022-11-29 22:54:21 -0500

    Initial commit

--- Sources/swift-aoc/Days/Day1.swift
+++ Sources/swift-aoc/Days/Day1.swift
@@ -10,7 +10,7 @@ import Foundation
 
 class Day1: Day {
     func part1(_ input: String) throws -> CustomStringConvertible {
-        throw NotImplementedError()
+        input.split(separator: "\n").compactMap { Int($0) }.adjacentPairs().count { $0 < $1 }
     }
     
     func part2(_ input: String) throws -> CustomStringConvertible {
