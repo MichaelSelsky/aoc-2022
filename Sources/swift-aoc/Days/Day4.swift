52efc116996e959bc49bc488c8d4d8ddf90b9cb0
Michael Selsky <michael@selsky.me>
2022-12-04 09:33:38 -0500

    Day 4

--- Sources/swift-aoc/Days/Day4.swift
+++ Sources/swift-aoc/Days/Day4.swift
@@ -7,10 +7,24 @@ import Foundation
 
 struct Day4: Day {
   func part1(_ input: String) throws -> CustomStringConvertible {
-    throw NotImplementedError()
+      input.lines().map { line in
+          let ranges = line.split(separator: ",").map { range in
+              let values = range.split(separator: "-")
+              let (start, end) = (values.first!, values.last!)
+              return Int(start)! ... Int(end)!
+          }
+          return ranges.first?.clamped(to: ranges.last!) == ranges.first || ranges.last?.clamped(to: ranges.first!) == ranges.last
+      }.count(where: { $0 })
   }
 
   func part2(_ input: String) throws -> CustomStringConvertible {
-    throw NotImplementedError()
+      input.lines().map { line in
+          let ranges = line.split(separator: ",").map { range in
+              let values = range.split(separator: "-")
+              let (start, end) = (values.first!, values.last!)
+              return Int(start)! ... Int(end)!
+          }
+          return ranges.first?.overlaps(ranges.last!) ?? false
+      }.count(where: { $0 })
   }
 }
