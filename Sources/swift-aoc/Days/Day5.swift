6e4482b442c176fff86ac1c5a4db6edc3256dfd5
Michael Selsky <michael@selsky.me>
2022-12-06 22:24:37 -0500

    Day 5 and 6

--- Sources/swift-aoc/Days/Day5.swift
+++ Sources/swift-aoc/Days/Day5.swift
@@ -6,11 +6,66 @@
 import Foundation
 
 struct Day5: Day {
-  func part1(_ input: String) throws -> CustomStringConvertible {
-    throw NotImplementedError()
-  }
-
-  func part2(_ input: String) throws -> CustomStringConvertible {
-    throw NotImplementedError()
-  }
+    func part1(_ input: String) throws -> CustomStringConvertible {
+        let stackEndIndex = input.firstIndex(of: "m")!
+        let stackDescription = input.prefix(upTo: stackEndIndex).trimmingCharacters(in: .newlines).lines().map(String.init)
+        let instructions = String(input.suffix(from: stackEndIndex)).lines()
+        let parsedInstructions = parseInstructions(instructions)
+        var stacks = parseStacks(stackDescription: stackDescription)
+        for instruction in parsedInstructions {
+            guard var s = stacks[instruction.1], var t = stacks[instruction.2] else { continue }
+            for _ in 0..<instruction.0 {
+                if let value = s.popLast() {
+                    t.append(value)
+                }
+            }
+            stacks[instruction.1] = s
+            stacks[instruction.2] = t
+        }
+        return stacks.sorted(by: { $0.key < $1.key }).compactMap { $0.value.last }.joined()
+    }
+    
+    func part2(_ input: String) throws -> CustomStringConvertible {
+        let stackEndIndex = input.firstIndex(of: "m")!
+        let stackDescription = input.prefix(upTo: stackEndIndex).trimmingCharacters(in: .newlines).lines().map(String.init)
+        let instructions = String(input.suffix(from: stackEndIndex)).lines()
+        let parsedInstructions = parseInstructions(instructions)
+        var stacks = parseStacks(stackDescription: stackDescription, reverseStacks: false)
+        for instruction in parsedInstructions {
+            guard var s = stacks[instruction.1], var t = stacks[instruction.2] else { continue }
+            let value = s.prefix(instruction.0)
+            t.insert(contentsOf: value, at: 0)
+            
+            s = Array(s.suffix(from: instruction.0))
+            stacks[instruction.1] = s
+            stacks[instruction.2] = t
+        }
+        return stacks.sorted(by: { $0.key < $1.key }).compactMap { $0.value.first }.joined()
+    }
+    
+    func parseStacks(stackDescription: [String], reverseStacks: Bool = true) -> [Int: [String]] {
+        let stackNumberLine = stackDescription.last!
+        var stacks: [Int: [String]] = [:]
+        for n in stackNumberLine {
+            if let i = Int(String(n)) {
+                let idx = stackNumberLine.firstIndex(of: n)
+                let s = stackDescription.prefix(stackDescription.count - 1).compactMap { line -> String? in
+                    guard let newIdx = idx?.samePosition(in: line), line[newIdx] != " " else { return nil }
+                    return String(line[newIdx])
+                }
+                if reverseStacks {
+                    let s = s.reversed()
+                }
+                stacks[i] = Array(s)
+            }
+        }
+        return stacks
+    }
+    
+    func parseInstructions(_ instructions: [Substring]) -> [(Int, Int, Int)] {
+        instructions.map { line -> (Int, Int, Int) in
+            let parts = line.split(separator: " ")
+            return (Int(parts[1])!, Int(parts[3])!, Int(parts[5])!)
+        }
+    }
 }
--- Sources/swift-aoc/Days/Day6.swift
+++ Sources/swift-aoc/Days/Day6.swift
@@ -6,12 +6,24 @@
 import Foundation
 
 struct Day6: Day {
-
-  func part1(_ input: String) throws -> CustomStringConvertible {
-    throw NotImplementedError()
-  }
-
-  func part2(_ input: String) throws -> CustomStringConvertible {
-    throw NotImplementedError()
-  }
+    
+    func part1(_ input: String) throws -> CustomStringConvertible {
+        findStartOf(input: input, length: 4)
+    }
+    
+    func part2(_ input: String) throws -> CustomStringConvertible {
+        findStartOf(input: input, length: 14)
+    }
+    
+    func findStartOf(input: String, length: Int) -> Int {
+        let windows = input.windows(ofCount: length)
+        var idx = length
+        for window in windows {
+            if Set(window).count == window.count {
+                return idx
+            }
+            idx += 1
+        }
+        return 0
+    }
 }
