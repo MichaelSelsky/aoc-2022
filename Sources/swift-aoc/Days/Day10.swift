e46cc8605638a2f717f28842f3617f8a4142de64
Michael Selsky <michael@selsky.me>
2022-12-10 11:47:40 -0500

    Day 10

--- Sources/swift-aoc/Days/Day10.swift
+++ Sources/swift-aoc/Days/Day10.swift
@@ -6,12 +6,68 @@
 import Foundation
 
 struct Day10: Day {
-
-  func part1(_ input: String) throws -> CustomStringConvertible {
-    throw NotImplementedError()
-  }
-
-  func part2(_ input: String) throws -> CustomStringConvertible {
-    throw NotImplementedError()
-  }
+    
+    func part1(_ input: String) throws -> CustomStringConvertible {
+        var register: Int = 1
+        let measureClocks = [20, 60, 100, 140, 180, 220]
+        var signalStrengths: [Int] = []
+        var clock: Int = 0 {
+            didSet {
+                if measureClocks.contains(clock) {
+                    signalStrengths.append(register * clock)
+                }
+            }
+        }
+        for line in input.lines() {
+            let commands = line.split(separator: " ")
+            switch commands.first! {
+            case "noop":
+                clock += 1
+            case "addx":
+                let val = Int(String(commands[1]))!
+                clock += 1
+                clock += 1
+                register += val
+            default:
+                continue
+            }
+        }
+        
+        return signalStrengths.sum
+    }
+    
+    func part2(_ input: String) throws -> CustomStringConvertible {
+        var register: Int = 1
+        let measureClocks = [20, 60, 100, 140, 180, 220]
+        var signalStrengths: [Int] = []
+        var display = ""
+        var clock: Int = -1 {
+            didSet {
+                let pos = clock % 40
+                if pos == 0 {
+                    display.append("\n")
+                }
+                if (register - 1 ... register + 1).contains(pos) {
+                    display.append("#")
+                } else {
+                    display.append(".")
+                }
+            }
+        }
+        for line in input.lines() {
+            let commands = line.split(separator: " ")
+            switch commands.first! {
+            case "noop":
+                clock += 1
+            case "addx":
+                let val = Int(String(commands[1]))!
+                clock += 1
+                clock += 1
+                register += val
+            default:
+                continue
+            }
+        }
+        return "\n" + display
+    }
 }
